c2038c7a169fb271a78234bf4c59eab1d44df655
Hasme Rafsanjani <hasmerafsanjani@gmail.com>
2023-11-09 02:52:03 +0530

    resume with out resposive

--- src/components/Main section/SkillSideBar.jsx
+++ src/components/Main section/SkillSideBar.jsx
@@ -1,20 +1,6 @@
 
 import React from 'react'
-import github from './Skills/github.jpg'
-import sql from './Skills/sql.jpg'
-import act from './Skills/react.png'
-import javaact from './Skills/java.jpg'
-import javasctipt from './Skills/javascript.png'
-import AI from './Skills/AI.png'
-import css from './Skills/css.png'
-import git from './Skills/photoshop.png'
-import phs from './Skills/img.png'
-import tailwind from './Skills/tailwind.png'
-import  premier from './Skills/premier.png'
-import  html from './Skills/html.png'
-import  figma from './Skills/figma.png'
-import  cad from './Skills/cad.png'
-import ubantu from './Skills/ubonto.jpg'
+
 
 
 
@@ -24,21 +10,35 @@ function SkillSideBar() {
         <h1 className='text-3xl text-center font-bold p-4 m-4'>Devlopment and Software Skills</h1>
       <div className="App text-center bg-green-400 mx-12 mb-5 px-4 py-7 shadow-[rgba(0,_0,_0,_0.4)_0px_30px_90px] rounded-2xl">
         <div className='gap-4 flex flex-wrap  justify-center  '>
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={github} alt="Github" />  
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={sql} alt="sql" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={act} alt="react" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={javaact} alt="java" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={javasctipt} alt="javascript" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={AI} alt="AI" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={css} alt="css" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={git} alt="photoshop" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={phs} alt="PS" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={tailwind} alt="tailwind" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={premier} alt="premier" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={html} alt="html" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={figma} alt="html" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={cad} alt="html" />
-          <img className='h-40 rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src={ubantu} alt="html" />
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src="https://www.webfx.com/wp-content/uploads/2022/08/github-logo.png" alt="Github" />  
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://static.tildacdn.com/tild6238-3035-4335-a333-306335373139/IMG_3349.jpg' alt="sql" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ0fX20XZ9WcCiM-_EINcthKnrldIsA2nNKAi77uAVmuxheapimECkmuEs2xebuUDx24s0" alt="react" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src="https://upload.wikimedia.org/wikipedia/commons/thumb/6/6a/JavaScript-logo.png/800px-JavaScript-logo.png"alt="javascript" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://logos-world.net/wp-content/uploads/2022/07/Java-Logo-700x394.png' alt="java" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://is1-ssl.mzstatic.com/image/thumb/Purple116/v4/26/fd/5b/26fd5bfb-d741-51df-8972-fab2e92a0521/AppIcon-0-1x_U007emarketing-0-7-0-sRGB-85-220.png/1200x630wa.png'alt="AI" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://ih1.redbubble.net/image.190911390.0447/st,small,507x507-pad,600x600,f8f8f8.u4.jpg' alt="css" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://blogs.incyclesoftware.com/hs-fs/hubfs/Git%20Logo.jpg?width=716&name=Git%20Logo.jpg' alt="git" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src="https://w7.pngwing.com/pngs/818/370/png-transparent-photoshop-2020-logo-icon.png" alt="PS" />
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://www.drupal.org/files/project-images/screenshot_361.png'alt="tailwind" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://e1.pngegg.com/pngimages/633/357/png-clipart-macos-app-icons-adobe-premiere.png' alt="premier" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://cdn-icons-png.flaticon.com/512/919/919827.png'alt="html" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://i.pinimg.com/736x/e1/78/1b/e1781bc61a928d061d15cd9f1d78fa88.jpg' alt="html" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://seeklogo.com/images/A/autocad-logo-69326D7728-seeklogo.com.png'alt="html" />
+
+          <img className='h-40 w-40 object-fill rounded-3xl hover:scale-110 hover:shadow-[0_4px_0px_rgb(0,0,0)] text-black bg-white ease-out hover:translate-y-1 transition-all' src='https://www.wizcase.com/wp-content/uploads/2022/07/en-ubuntu-logo.jpg'alt="html" />
+
         </div>
       </div>
       </>
